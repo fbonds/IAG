028eb49dfa18e0299eb136f5ffc34561dd0eb6a4
Fletcher Bonds <fbonds@gmail.com>
2023-02-22 08:20:39 -0800

    Update inflight-with-data-overlay.swift

--- inflight-with-data-overlay.swift
+++ inflight-with-data-overlay.swift
@@ -1,1 +1,50 @@
+import AVFoundation
+import CoreMotion
+
+class FlightRecorder {
+  
+  let motionManager = CMMotionManager()
+  let session = AVCaptureSession()
+  var device: AVCaptureDevice?
+  var videoOutput = AVCaptureVideoDataOutput()
+  var audioOutput = AVCaptureAudioDataOutput()
+  var initialTimestamp: TimeInterval = 0.0
+  var initialVelocity: Double = 0.0
+  var initialPosition: Double = 0.0
+  
+  func startRecording() {
+    guard motionManager.isAccelerometerAvailable, motionManager.isBarometerAvailable else {
+      print("Accelerometer and/or barometer is not available on this device")
+      return
+    }
+    
+    // Set up the video and audio capture
+    // ...
+    
+    motionManager.startAccelerometerUpdates(to: .main) { [weak self] (accelerometerData, error) in
+      guard let self = self else { return }
+      guard let accelerometerData = accelerometerData else {
+        print("Error accessing accelerometer data: \(error?.localizedDescription ?? "Unknown error")")
+        return
+      }
+      
+      let acceleration = accelerometerData.acceleration
+      let timestamp = accelerometerData.timestamp
+      // Use the acceleration and timestamp to calculate the velocity and position of the device over time
+      // ...
+    }
+    
+    motionManager.startBarometerUpdates(to: .main) { [weak self] (barometerData, error) in
+      guard let self = self else { return }
+      guard let barometerData = barometerData else {
+        print("Error accessing barometer data: \(error?.localizedDescription ?? "Unknown error")")
+        return
+      }
+      
+      let pressure = barometerData.pressure
+      // Use the pressure to determine the altitude of the device
+      // ...
+    }
+  }
+}
 
