50711bc713da524e4460f1960a1b43db6a2e9c57
Fletcher Bonds <fbonds@gmail.com>
2023-02-22 08:20:02 -0800

    Update upload-to-tiktok.swift

--- upload-to-tiktok.swift
+++ upload-to-tiktok.swift
@@ -1,1 +1,22 @@
+import Alamofire
 
+class TikTokUploader {
+  
+  func uploadVideo(videoURL: URL) {
+    let headers: HTTPHeaders = [
+      "Content-Type": "multipart/form-data"
+    ]
+    
+    AF.upload(multipartFormData: { (multipartFormData) in
+      multipartFormData.append(videoURL, withName: "video")
+    }, to: "https://api.tiktok.com/upload", method: .post, headers: headers)
+      .responseJSON { response in
+        switch response.result {
+        case .success(let value):
+          print("Successfully uploaded video: \(value)")
+        case .failure(let error):
+          print("Failed to upload video: \(error)")
+        }
+    }
+  }
+}
